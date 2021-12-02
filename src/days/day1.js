fa9cc21ceaceada793c7326fec0b5384da06f834
Andy O'Neill <aoneill01@gmail.com>
2021-12-01 21:33:24 -0500

    Day 1

--- src/days/day1.js
+++ src/days/day1.js
@@ -1,9 +1,79 @@
-import getInput from "../helpers/getInput.js"
+import { getInputAsyncGenerator } from '../helpers/getInput.js'
 
 export async function part1() {
-  // const input = await getInput(1)
+  const input = getInputAsyncGenerator(1)
+  console.log(await countIncreases(mapIter(input, toDepth)))
 }
 
 export async function part2() {
-  // const input = await getInput(1)
+  const input = getInputAsyncGenerator(1)
+  // const input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+  console.log(await countIncreases(windowGenerator(mapIter(input, toDepth), 3)))
 }
+
+async function countIncreases(values) {
+  const iterator = (values[Symbol.asyncIterator] ?? values[Symbol.iterator]).call(values)
+  let increaseCount = 0
+  let previous = (await iterator.next())?.value
+
+  for await (const next of iterator) {
+    if (next > previous) increaseCount++
+    previous = next
+  }
+
+  return increaseCount
+}
+
+async function* windowGenerator(values, windowLength) {
+  const iterator = (values[Symbol.asyncIterator] ?? values[Symbol.iterator]).call(values)
+  const history = []
+
+  let window = 0
+
+  for (let i = 0; i < windowLength; i++) {
+    const next = await iterator.next()
+    if (next.done) return
+    window += next.value
+    history.push(next.value)
+  }
+
+  yield window
+
+  let i = windowLength - 1
+
+  for await (const value of iterator) {
+    i = (i + 1) % windowLength
+    window = window - history[i] + value
+    history[i] = value
+    yield window
+  }
+}
+
+const toDepth = (value) => parseInt(value, 10)
+
+async function* mapIter(iter, callback) {
+  for await (const value of iter) {
+    yield callback(value)
+  }
+}
+
+// function tap(iter) {
+//   return mapIter(iter, (value) => {
+//     console.log(value)
+//     return value
+//   })
+// }
+
+// export async function part2() {
+//   const input = await getInput(1)
+//   const depths = input.map(toDepth)
+//   let increases = 0
+//   for (let i = 0; i < depths.length - 3; i++) {
+//     const window1 = depths[i] + depths[i + 1] + depths[i + 2]
+//     const window2 = depths[i + 1] + depths[i + 2] + depths[i + 3]
+//     if (window1 < window2) {
+//       increases++
+//     }
+//   }
+//   console.log(increases)
+// }
--- src/helpers/getInput.js
+++ src/helpers/getInput.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 import dotenv from 'dotenv'
 dotenv.config()
 
-export default async function getInput(day) {
+export async function getInput(day) {
   const response = await fetch(`https://adventofcode.com/2021/day/${day}/input`, {
     headers: {
       accept: 'text/html',
@@ -15,4 +15,29 @@ export default async function getInput(day) {
 
   const lines = text.split('\n')
   return lines[lines.length - 1] === '' ? lines.slice(0, lines.length - 1) : lines
+}
+
+export async function* getInputAsyncGenerator(day) {
+  const response = await fetch(`https://adventofcode.com/2021/day/${day}/input`, {
+    headers: {
+      accept: 'text/html',
+      cookie: process.env.COOKIE,
+    },
+    method: 'GET',
+  })
+
+  let previous = ''
+  for await (const chunk of response.body) {
+    previous += chunk
+    while (true) {
+      const eolIndex = previous.indexOf('\n')
+      if (eolIndex < 0) break
+      const line = previous.slice(0, eolIndex + 1)
+      yield line.trim()
+      previous = previous.slice(eolIndex + 1)
+    }
+  }
+  if (previous.length > 0) {
+    yield previous
+  }
 }
